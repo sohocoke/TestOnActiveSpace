0310cc461bf7f12d8ce61d0a94de05ace43fec4e
Andy Park <andy@bigbearlabs.com>
2015-07-31 09:16:18 +0200

    Initial Commit

---
+++ TestOnActiveSpace/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TestOnActiveSpace
+//
+//  Created by ilo on 31/07/2015.
+//  Copyright (c) 2015 Big Bear Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
